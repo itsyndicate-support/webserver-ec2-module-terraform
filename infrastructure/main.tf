2d138e8fed424c61b2ebf3a33a0d77df5ad1ef44
vitaliy <howery1337@gmail.com>
2023-06-01 15:17:40 +0300

    add s3 for tfstate

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,11 @@
 provider "aws" {
   region = "eu-central-1"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "my-terraform-state-bucket"
+    key    = "terraform.tfstate"
+    region = "eu-central-1"
+  }
+}
