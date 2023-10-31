499a9708d7c4827bac7b17d5853bcc5e06d1d1c5
wexire <ppo.devops@gmail.com>
2023-10-31 02:52:24 +0200

    update config

--- test/install_terraform.sh
+++ test/install_terraform.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+apt-get update && apt-get install -y gnupg software-properties-common
 
 wget -O- https://apt.releases.hashicorp.com/gpg | \
 gpg --dearmor | \
-sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
 
 gpg --no-default-keyring \
 --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
@@ -11,8 +11,8 @@ gpg --no-default-keyring \
 
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
 https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
-sudo tee /etc/apt/sources.list.d/hashicorp.list
+tee /etc/apt/sources.list.d/hashicorp.list
 
-sudo apt update
+apt update
 
-sudo apt-get install terraform -y
+apt-get install terraform -y
