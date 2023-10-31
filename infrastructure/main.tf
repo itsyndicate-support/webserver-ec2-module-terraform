e14498dc1908f47c14d9868cceedabc2dc8591e4
yanchoys <98917290+yanchoys@users.noreply.github.com>
2023-10-31 04:24:04 +0200

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   profile = "default"
   region = "eu-central-1"
-  access_key_id = AKIATBVJ2SY3OJFDYIQN
-  secret_access_key = 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
+  access_key = AKIATBVJ2SY3OJFDYIQN
+  secret_key = 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
 }
